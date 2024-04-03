ba6db5bdf4743b2e72b69e1adec3438e5c19cef8
Hopeson <hopesonogaga@gmail.com>
2024-04-03 12:45:06 +0100

    first form javascript

--- build/js/index.js
+++ build/js/index.js
@@ -1,4 +1,30 @@
-document.getElementById('toggle').addEventListener('change', function() {
-    const label = document.getElementById('toggleLabel');
-    label.textContent = this.checked ? 'Yearly' : 'Month';
-  });
+const formButton = document.getElementById("form");
+
+formButton.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  // Move these lines inside the submit event listener
+  const phone_number = document.getElementById("phone-number").value;
+  const user_name = document.getElementById("name").value;
+  const email = document.getElementById("email").value;
+
+  let user_info = {
+    phone_number: phone_number,
+    name: user_name,
+    email: email,
+  };
+
+  console.log(user_info);
+
+  // local storage
+
+  localStorage.setItem("user_info", JSON.stringify(user_info));
+
+  //next page
+  location.href = "./pick.html";
+});
+
+// document.getElementById("toggle").addEventListener("change", function () {
+//   const label = document.getElementById("toggleLabel");
+//   label.textContent = this.checked ? "Yearly" : "Month";
+// });
