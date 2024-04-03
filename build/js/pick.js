3376b8caad600c9dfd0efbec2cab9068c5eb296b
Hopeson <hopesonogaga@gmail.com>
2024-04-03 14:16:45 +0100

    new features

--- build/js/pick.js
+++ build/js/pick.js
@@ -1,3 +1,8 @@
+const arcade = document.getElementById('arcade')
+const advance = document.getElementById('advance')
+const pro = document.getElementById('pro')
+
+
 document.getElementById("toggle").addEventListener("change", function () {
   const label = document.getElementById("toggleLabel");
   label.textContent = this.checked ? "Yearly" : "Month";
