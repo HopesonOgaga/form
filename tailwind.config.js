bd79bdb20278cf152ef19ac72e46909346c24e04
Hopeson <hopesonogaga@gmail.com>
2024-04-04 13:44:13 +0100

    fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./build/*.html", "./build/js/*.js",],
+  content: ["./build/*.html", "./build/js/*.js"],
   theme: {
     extend: {},
+    fontFamily: {
+      'mono': ["Reddit Mono", "monospace"] 
+    },
   },
   plugins: [],
-}
-
+};
